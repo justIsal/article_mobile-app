1ba99c417c9c55e45d3c15d0d50fab6677569d83
justIsal <tsalmanisal@gmail.com>
2024-05-18 09:51:59 +0700

    setup pages

---
+++ lib/pages/articles/articles_detail_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ArticleDetailScreen extends StatefulWidget {
+  const ArticleDetailScreen({super.key});
+
+
+  @override
+  _ArticleDetailScreenState createState() => _ArticleDetailScreenState();
+}
+
+class _ArticleDetailScreenState extends State<ArticleDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('ArticleDetailScreen screen');
+  }
+}
---
+++ lib/pages/save/save_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SaveScreen extends StatefulWidget {
+  const SaveScreen({super.key});
+
+
+  @override
+  _SaveScreenState createState() => _SaveScreenState();
+}
+
+class _SaveScreenState extends State<SaveScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('SaveScreen screen');
+  }
+}
